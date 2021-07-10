ab00b9061254952d6fb4d4ca52b9d04c58e3f2a3
chacha86 <taejincha0619@gmail.com>
2021-07-10 17:30:42 +0900

    게시물 수정/삭제

---
+++ day7_jsp/list.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@ page import="jsp_day6.ArticleDao" %>
+<%@ page import="java.util.ArrayList" %>
+<%@ page import="jsp_day6.Article" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+<h1> 게시물 목록 </h1>
+<%@ include file="topMenu.jsp" %>
+<!-- 출력부 -->
+<c:forEach items="${articles}" var="article">
+	번호 : ${article.id} <br>
+	제목 : <a href="/JSP/article7/detail?articleId=${article.id}">${article.title} </a><br>
+	<hr>
+</c:forEach>
+<a href="/JSP/jsp/day7/addForm.jsp">글쓰기</a>
+</body>
+</html>
---
+++ day7_src/ArticleDao.java
@@ -0,0 +1,118 @@
+package jsp_day7;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public class ArticleDao {
+	String url = "jdbc:mysql://127.0.0.1:3306/jsp?serverTimezone=UTC";
+	String user = "sbsst";
+	String password = "sbs123414";
+	
+	public Connection getConnection() throws ClassNotFoundException, SQLException {
+		Class.forName("com.mysql.cj.jdbc.Driver");
+		Connection conn = DriverManager.getConnection(url, user, password);		
+		return conn;
+	}
+	
+	public Article getArticleById(int articleId) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		
+		String sql = "SELECT a.id, a.title, a.body, a.regDate, m.name, a.hit, a.memberId\r\n" + 
+				"FROM article a \r\n" + 
+				"INNER JOIN `member` m\r\n" + 
+				"ON a.memberId = m.id where a.id = " + articleId;
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		ArrayList<Article> articleList = new ArrayList<>();
+		
+		if(rs.next()) {
+			int id = rs.getInt("id");
+			String title = rs.getString("title");
+			String body = rs.getString("body");
+			String regDate = rs.getString("regDate");
+			int hit = rs.getInt("hit");
+			int memberId = rs.getInt("memberId");
+			String name = rs.getString("name");
+			
+			Article a = new Article(id, regDate, title, body, name, hit, memberId);
+			
+			return a;
+		}
+		
+		return null;
+	}
+	
+	public ArrayList<Article> getArticleList() throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		
+		String sql = "SELECT a.id, a.title, a.body, a.regDate, m.name, a.hit, a.memberId\r\n" + 
+				"FROM article a \r\n" + 
+				"INNER JOIN `member` m\r\n" + 
+				"ON a.memberId = m.id";
+		
+		ResultSet rs = stmt.executeQuery(sql);
+		
+		ArrayList<Article> articleList = new ArrayList<>();
+		
+		while(rs.next()) {
+			int id = rs.getInt("id");
+			String title = rs.getString("title");
+			String body = rs.getString("body");
+			String regDate = rs.getString("regDate");
+			String name = rs.getString("name");
+			int hit = rs.getInt("hit");
+			int memberId = rs.getInt("memberId");
+			
+			Article a = new Article(id, title, body, regDate, name, hit, memberId);
+			
+			articleList.add(a);
+		}
+		
+		return articleList;
+	}
+	
+	public void insertArticle(String title, String body) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		
+		String sql = "INSERT INTO article\r\n" + 
+				"SET regDate = NOW(),\r\n" + 
+				"title = '" + title + "',\r\n" + 
+				"`body` = '" + body + "',\r\n" + 
+				"boardId = 1, \r\n" + 
+				"memberId = 1,\r\n" + 
+				"writer = '',\r\n" + 
+				"passwd = '',\r\n" + 
+				"hit = 0";
+		
+		stmt.executeUpdate(sql);
+	}
+
+	public void updateArticle(int id, String title, String body) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		
+		String sql = "UPDATE article SET title = '" + title + "', `body` = '" + body + "' WHERE id = " + id;
+		
+		stmt.executeUpdate(sql);
+		
+	}
+
+	public void deleteArticle(int id) throws ClassNotFoundException, SQLException {
+		Connection conn = getConnection();
+		Statement stmt = conn.createStatement();
+		
+		String sql = "DELETE FROM article WHERE id = " + id;
+		
+		stmt.executeUpdate(sql);
+	}
+	
+}
+
